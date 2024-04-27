b526a5a7d3ef909026feb39e4fd47ff44a8bc60d
Hidaya Wetsakhulia <hidayawetsa@gmail.com>
2024-04-27 20:32:14 +0300

    commit

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  //initialising state
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick(){
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn? "black" : "white";
+  return <button style={{background: color}} onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
